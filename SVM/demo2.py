e07ae09caaf42894b7e664efc7245a31be70d372
scottxie123 <1658755776@qq.com>
2020-07-08 10:19:15 +0800

    update demo2

--- SVM/demo2.py
+++ SVM/demo2.py
@@ -10,7 +10,7 @@ t = time()
 X_train,y_train,X_test,y_test = gen_data()
 print("Done!",time()-t)
 
-svm = SVM(X_train.T,y_train,C = 1,toler = 0.001,kernel = laplacian_kernel,parameter=[1])
+svm = SVM(X_train.T,y_train,C = 1,toler = 0.001,kernel = laplacian_kernel,parameter=[0.1])
 print('Training...')
 t = time()
 svm.fit()
@@ -29,11 +29,33 @@ for i in range(X_test.T.shape[0]):
 results = np.array(results)
 print('accuracy: ',np.sum(results)/len(results))
 
-x = np.linspace(-4,4)
+x = np.linspace(-2,5)
 y = -1*(w[0]*x-svm.b)/w[1]
-plt.scatter(X_train[0][:1000],X_train[1][:1000])
-plt.scatter(X_train[0][1000:],X_train[1][1000:])
+plt.figure()
+plt.scatter(X_train[0][:1000],X_train[1][:1000],alpha=0.4, lw=0)
+plt.scatter(X_train[0][1000:],X_train[1][1000:],alpha=0.4, lw=0)
 for i in np.nonzero(svm.alpha)[0]:
     plt.scatter(X_train[0][i],X_train[1][i],marker='+',c='k')
 plt.plot(x,y)
+#plt.show()
+
+
+x_1 = np.linspace(-3.5,8,num=200)
+y_1 = np.linspace(-3.5,8,num=200)
+[xx,yy] = np.meshgrid(x_1,y_1)
+xx = xx.reshape(-1)
+yy = yy.reshape(-1)
+result = []
+for x_2,y_2 in zip(xx,yy):
+    pre = svm.predict(np.array([x_2,y_2]))>0
+    result.append(pre)
+ind = np.nonzero(result)[0]
+ind2 = np.nonzero([not i for i in result])[0]
+#print(ind)
+plt.figure()
+plt.plot(x,y,c='k')
+plt.scatter(X_test[0][:1000],X_test[1][:1000],marker='+',c='k')
+plt.scatter(X_test[0][1000:],X_test[1][1000:],marker='*',c='r')
+plt.scatter(xx[ind],yy[ind],c = 'b',alpha=0.1, lw=0)
+plt.scatter(xx[ind2],yy[ind2],c = 'g',alpha=0.1, lw=0)
 plt.show()
